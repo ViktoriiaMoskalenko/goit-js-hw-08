efcfadda60f89dc5ac1ee09be43cf33a2e360158
Viktoriia Moskalenko <vika01mos@gmail.com>
2022-11-12 13:58:10 +0200

    add file

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -6,38 +6,15 @@ const player = new Player(iframe);
 
 let i = Number(localStorage.getItem('videoplayer-current-time')) || 0;
 
-// player.on(
-//   'timeupdate',
-//   throttle(function (event) {
-//     i += 1;
-//     localStorage.setItem('videoplayer-current-time', i - 1);
-//     console.log(event.setcurrentTime);
-//   }, 1000)
-// );
-
 player.on(
   'timeupdate',
   throttle(function (event) {
     localStorage.setItem(
       'videoplayer-current-time',
-      JSON.stringify(event.seconds)
+      JSON.stringify(event.seconds),
+      console.log(event.seconds)
     );
   }, 1000)
 );
 
-player
-  .setCurrentTime(i)
-  .then(function (seconds) {
-    // seconds = the actual time that the player seeked to
-  })
-  .catch(function (error) {
-    switch (error.name) {
-      case 'RangeError':
-        // the time was less than 0 or greater than the video’s duration
-        break;
-
-      default:
-        // some other error occurred
-        break;
-    }
-  });
+player.setCurrentTime(i);
